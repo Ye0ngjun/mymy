2bfe93dd523abced79e81293bb69e3c53269f1ea
Yeongjun Joo <wndudwns6824@naver.com>
2024-03-08 02:20:49 +0900

    feat : cart, payment 만듦

---
+++ app/cart/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      <div className="text-center text-white">현대카드 무이자 이벤트 중</div>
+      {children}
+    </div>
+  );
+}
---
+++ app/cart/payment/page.js
@@ -0,0 +1,7 @@
+export default function payment() {
+  return (
+    <div>
+      <div className="mt-10 text-center text-white">결제페이지입니다.</div>
+    </div>
+  );
+}
